f8fc47e56a887642195962d2fd5860ba766d54c2
Nathan Smith <nathan@nathan-smith.org>
2021-05-13 23:24:10 -0400

    v0.1.0

--- main.ts
+++ main.ts
@@ -1,112 +1,137 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+  App,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+  WorkspaceLeaf,
+} from "obsidian";
 
-interface MyPluginSettings {
-	mySetting: string;
+interface CollapseAllSettings {
+  maxDepth: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: CollapseAllSettings = {
+  maxDepth: -1,
+};
+
+export default class CollapseAllPlugin extends Plugin {
+  settings: CollapseAllSettings;
+
+  async onload() {
+    await this.loadSettings();
+
+    // TODO: Find a better event to listen for.
+    // We only add the button once, but this is probably unnecessary.
+    this.app.workspace.on("active-leaf-change", () => {
+      const explorers = this.getFileExplorers();
+      explorers.forEach((exp) => {
+        this.addCollapseButton(exp);
+      });
+    });
+
+    this.addSettingTab(new CollapseAllSettingsTab(this.app, this));
+  }
+
+  onunload() {
+    console.log("unloading plugin");
+  }
+
+  private getFileExplorers(): WorkspaceLeaf[] {
+    return this.app.workspace.getLeavesOfType("file-explorer");
+  }
+
+  private addCollapseButton(explorer: WorkspaceLeaf): void {
+    // TODO: containerEl is not a public property of the leaf. Is there a better way?
+    const container = (explorer as any).containerEl as HTMLDivElement;
+    const navContainer = container.querySelector(
+      "div.nav-buttons-container"
+    ) as HTMLDivElement;
+    if (navContainer.querySelector(".collapse-button")) {
+      return;
+    }
+    const newIcon = document.createElement("div");
+    // TODO: Add an actual icon
+    newIcon.innerHTML = "C";
+    newIcon.className = "nav-action-button collapse-button";
+    this.registerDomEvent(newIcon, "click", () => {
+      const exp = document.querySelector(
+        'div.workspace-leaf-content[data-type="file-explorer"]'
+      ) as HTMLDivElement | null;
+      if (exp) {
+        this.collapseAllUnder(exp, 0);
+      }
+    });
+    navContainer.appendChild(newIcon);
+  }
+
+  private collapseAllUnder(currentFolder: HTMLDivElement, depth: number): void {
+    while (true) {
+      // Depth first search
+      const nextFolder = this.getNextOpenFolder(currentFolder);
+      // Root element has children to close
+      if (
+        nextFolder !== null &&
+        (this.settings.maxDepth === -1 || depth < this.settings.maxDepth)
+      ) {
+        this.collapseAllUnder(nextFolder, depth + 1);
+      }
+      // Root element has no children to close. Close root element.
+      else {
+        const title = currentFolder.querySelector(
+          "div.nav-folder-title"
+        ) as HTMLDivElement | null;
+        const children = currentFolder.querySelector("div.nav-folder-children");
+        if (title && children) {
+          // TODO: Fix incompatibility with folder note plugin.
+          // As is, it will create new notes for some reason.
+          title.click();
+        }
+        break;
+      }
+    }
+  }
+
+  private getNextOpenFolder(
+    rootElement: HTMLElement | Document
+  ): HTMLDivElement | null {
+    return rootElement.querySelector(
+      "div.nav-folder:not(.is-collapsed):not(.mod-root)"
+    ) as HTMLDivElement;
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+// TODO: Split this into its own file.
+class CollapseAllSettingsTab extends PluginSettingTab {
+  plugin: CollapseAllPlugin;
 
-	async onload() {
-		console.log('loading plugin');
+  constructor(app: App, plugin: CollapseAllPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-		await this.loadSettings();
-
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
-
-		this.addStatusBarItem().setText('Status Bar Text');
-
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
-		});
-
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-		console.log('unloading plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  // TODO: Setting the depth doesn't actually work.
+  display(): void {
+    let { containerEl } = this;
+    containerEl.empty();
+    new Setting(containerEl)
+      .setName("Max Depth")
+      .setDesc(
+        "Collapsing is done recursively. If you have an excessive amount of child folders, set this value. -1 is unlimited."
+      )
+      .addText((text) => {
+        text.setValue("-1").onChange(async (value) => {
+          const depth = parseInt(value, 10);
+          this.plugin.settings.maxDepth = depth ? depth : -1;
+          await this.plugin.saveSettings();
+        });
+      });
+  }
 }
