a950c303c82c58c3487381bfab40316083843477
Michael Naumov <mnaoumov@gmail.com>
2022-07-08 20:49:35 -0400

    Extract TagExplorerItemImpl

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -24,6 +24,23 @@ export interface TagExplorerItem {
   };
 }
 
+export class TagExplorerItemImpl implements TagExplorerItem {
+  constructor(item: TagExplorerItem) {
+    Object.assign(this, item);
+    this.setCollapsed = item.setCollapsed.bind(item);
+  }
+
+  tag: string;
+  children?: TagExplorerItem[];
+  collapsed?: boolean;
+  setCollapsed?: (state: boolean) => void;
+  vChildren?: { _children: TagExplorerItem[]; };
+
+  getChildrenSafe(): TagExplorerItem[] {
+    return this.children ?? this.vChildren._children;
+  }
+}
+
 export interface TagPaneView extends View {
   tagDoms: TagExplorerItem[]
 }
--- src/provider/tag-pane.ts
+++ src/provider/tag-pane.ts
@@ -1,7 +1,7 @@
 import { WorkspaceLeaf } from 'obsidian';
 import { CollapseAllPlugin } from '../plugin';
 import { ProviderType } from '../constants';
-import { TagExplorerItem, TagPaneView } from '../interfaces';
+import { TagExplorerItem, TagExplorerItemImpl, TagPaneView } from '../interfaces';
 import { ProviderBase } from './base';
 
 
@@ -27,7 +27,7 @@ export class TagPaneProvider extends ProviderBase {
 
     // Collapse / expand
     items.forEach((item) => {
-      if (this.getChildrenSafe(item).length > 0 && item.collapsed !== collapsed) {
+      if (item.getChildrenSafe().length > 0 && item.collapsed !== collapsed) {
         item.setCollapsed(collapsed);
       }
     });
@@ -40,18 +40,14 @@ export class TagPaneProvider extends ProviderBase {
   /**
    * Get the root tag pane items from the tag pane view. This property is not documented.
    */
-  private getTagItems(tagPane: WorkspaceLeaf): TagExplorerItem[] {
-    return Object.values((tagPane.view as TagPaneView).tagDoms);
+  private getTagItems(tagPane: WorkspaceLeaf): TagExplorerItemImpl[] {
+    return Object.values((tagPane.view as TagPaneView).tagDoms).map((item: TagExplorerItem) => new TagExplorerItemImpl(item));
   }
 
   /**
    * Given the root tags, checks all children to confirm they are closed. Note that this is recursive.
    */
-  private tagsAreCollapsed(items: TagExplorerItem[]): boolean {
-    return items.every((i) => this.getChildrenSafe(i).length === 0 || i.collapsed === true);
-  }
-
-  private getChildrenSafe(item: TagExplorerItem): TagExplorerItem[] {
-    return item.children ?? item.vChildren._children;
+  private tagsAreCollapsed(items: TagExplorerItemImpl[]): boolean {
+    return items.every((i) => i.getChildrenSafe().length === 0 || i.collapsed === true);
   }
 }
